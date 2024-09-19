cc956558bcb8df9324df3fb570c7c049b80c28aa
GARajab <mohamed.rajab@ewa.bh>
2024-09-19 19:07:32 +0300

    array2

---
+++ app.js
@@ -0,0 +1,43 @@
+// let courseGrade1 = 100
+// let courseGrade2 = 90
+// let courseGrade3 = 88
+// let courseGrade4 = 77
+// let courseGrade5 = 78
+// let courseGrade6 = 68
+// let courseGrade7 = 46
+// let courseGrade8 = 88
+// let courseGrade9 = 44
+
+// for (let i = 1; (i = 9); i++) {
+//   let courseGrade = courseGrade[i] + courseGrade
+//   console.log(courseGrade)
+// }
+
+// const Array = [0, 1, 2, 3, 4, 5]
+// Array.pop()
+// console.log(Array)
+// Array.pop()
+// Array.pop()
+// console.log(Array)
+
+// hightest = 0
+// for (const grade of grades) {
+//   if (hightest < grade) {
+//     hightest = grade
+//   }
+// }
+// consol.log(hightest)
+// // this new for loop called for of used with arrays
+
+// Array.forEach((grade) => {
+//   consol.log(grade * 2)
+// })
+// Array[0] = ""
+// console.log(Array)
+books = []
+books = ["The Shining", "Pride", "Prejudice", "Book1", "Book2"]
+console.log(books[1])
+books[1] = "Dune"
+books.forEach((book) => {
+  console.log(book)
+})
